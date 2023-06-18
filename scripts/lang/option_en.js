adfa2d60adf5a52276d3cf6d5670f1fb2a8c0f89
Baptiste Lepers <baptiste.lepers@gmail.com>
2023-06-18 20:26:06 +0200

    Description of the plugin

--- scripts/lang/option_en.js
+++ scripts/lang/option_en.js
@@ -1,4 +1,5 @@
 known_sentences.concat({
+   'GPX Mapbox Isochrones':'GPX Mapbox Isochrones<br/><p style="font-size:12px;font-style:normal">Display your GPX traces on top of isochrone maps. See https://github.com/BLepers/isochrones-gtfs to generate the tilesets. Then go to <a href="../#!isomap">the isomap</a>. Requires the GPX Map plugin to work.',
    gpxmapgl_activated:'Activate GPX GL Map plugin',
    gpxmapgl_token:'Mapbox Public Token',
    gpxmapgl_tileset:'Mapbox Tileset to display',
--- scripts/lang/option_fr.js
+++ scripts/lang/option_fr.js
@@ -1,4 +1,5 @@
 known_sentences.concat({
+   'GPX Mapbox Isochrones':'GPX Mapbox Isochrones<br/><p style="font-size:12px;font-style:normal">Affiche vos traces GPX sur une carte isochrone de transport en commun. Voir https://github.com/BLepers/isochrones-gtfs pour générer le tileset. Ensuite vous pouvez <a href="../#!isomap">afficher la carte</a>. Nécessite le plugin GPX Map pour fonctionner.',
    gpxmapgl_activated:'Activer le plugin GPX GL Map',
    gpxmapgl_token:'Mapbox Public Token',
    gpxmapgl_tileset:'Mapbox Tileset à afficher',
